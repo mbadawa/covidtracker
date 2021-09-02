aa30539f5eead050eef3514a17d1aa8c23d57a75
Mohamad Badawa <7badawa@gmail.com>
2021-09-02 16:38:30 -0400

    Tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { BsPlusSquare } from "react-icons/bs";
+import { RiDeviceRecoverLine } from "react-icons/ri";
+import { GiDeathSkull } from "react-icons/gi";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const axios = require("axios").default;
+	const [confirmedCases, setConfirmedCases] = useState(0);
+	const [recoveredCases, setRecoveredCases] = useState(0);
+	const [deaths, setDeaths] = useState(0);
+	const [lastUpdate, setLastUpdate] = useState(0);
+	axios.get("https://covid19.mathdro.id/api").then(function (response) {
+		// Confiremd Cases
+		setConfirmedCases(response.data.confirmed.value);
+		// Recovered Cases
+		setRecoveredCases(response.data.recovered.value);
+		// Deaths
+		setDeaths(response.data.deaths.value);
+		// Last Updated
+		setLastUpdate(response.data.lastUpdate);
+	});
+	return (
+		<div className="bg-gray-700 h-screen grid grid-rows-4 p-5 gap-2">
+			<div className="bg-gray-600 w-1/2 shadow-inner rounded-lg flex flex-col justify-center items-center ml-auto mr-auto gap-2">
+				<p className="text-lg font-bold text-gray-300">Confirmed Cases</p>
+				<p className="flex items-center gap-2 text-lg text-gray-300">
+					<BsPlusSquare />
+					{confirmedCases}
+				</p>
+			</div>
+			<div className="bg-gray-600 w-1/2 shadow-inner rounded-lg flex flex-col justify-center items-center ml-auto mr-auto gap-2">
+				<p className="text-lg font-bold text-gray-300">Recovered</p>
+				<p className="flex items-center gap-2 text-lg text-gray-400">
+					<RiDeviceRecoverLine />
+					{recoveredCases}
+				</p>
+			</div>
+			<div className="bg-gray-600 w-1/2 shadow-inner rounded-lg flex flex-col justify-center items-center ml-auto mr-auto gap-2">
+				<p className="text-lg font-bold text-gray-300">Deaths</p>
+				<p className="flex items-center gap-2 text-lg text-gray-400">
+					<GiDeathSkull />
+					{deaths}
+				</p>
+			</div>
+			<p className="flex items-center justify-center text-gray-400">
+				Last Updated : {lastUpdate}
+			</p>
+		</div>
+	);
 }
 
 export default App;
